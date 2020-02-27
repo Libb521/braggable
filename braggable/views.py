fb93154ec172495531c30495fa7d9c538a4230fb
Libb521 <eotieno39@yahoo.com>
2020-02-27 11:14:15 +0300

    created the project and the app then imported httpResponse in the views.py file

--- braggable/views.py
+++ braggable/views.py
@@ -1,6 +1,8 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.http import HttpResponse
+# from __future__ import unicode_literals
 
 from django.shortcuts import render
 
 # Create your views here.
+def brag(request):
+    return HttpResponse('My braggable Apps so far')
