5c51a03faa5963e65d2851e96b34560f9f9f286c
Libb521 <eotieno39@yahoo.com>
2020-02-27 15:13:34 +0300

    working on the html templates to have my portfolio displayed

--- braggable/models.py
+++ braggable/models.py
@@ -13,3 +13,17 @@ class Project(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(max_length=60, unique=True)
+
+    class tags(models.Model):
+        name = models.CharField(max_length =30)
+
+    def __unicode__(self):
+        return self.title
+
+class ProjectImage(models.Model):
+    image = models.ImageField(upload_to='albums', null=True)
+    # album = models.ForeignKey('album', on_delete=models.PROTECT)
+    width = models.IntegerField(default=0)
+    height = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    
