e66908f09c4be043def6ad7f28db9d2778527c87
Libb521 <eotieno39@yahoo.com>
2020-02-27 15:11:25 +0300

    rectified the URL paths for me to be able to load braggables as my home page

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^braggable/', include('braggable.urls'))
+    url(r'^$', include('braggable.urls'))
 ]
