df91ee083f3252cec59d04d376e0979b48235b46
Libb521 <eotieno39@yahoo.com>
2020-02-27 12:13:34 +0300

    created a welcome.html file to dispaly on the server when loaded

--- braggable/views.py
+++ braggable/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def brag(request):
-    return HttpResponse('My braggable Apps so far')
+    return render(request, 'welcome.html')
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'braggable',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
